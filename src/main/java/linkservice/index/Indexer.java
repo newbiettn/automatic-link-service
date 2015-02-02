918cd1bcc58f10c6c0d629dc68966b666329e898
newbiettn <newbiettn@gmail.com>
2015-02-02 21:40:57 +0100

    Add Indexer

---
+++ src/main/java/linkservice/index/Indexer.java
@@ -0,0 +1,115 @@
+package linkservice.index;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collection;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+public class Indexer {
+	//log4j
+	static Logger log = Logger.getLogger(Indexer.class.getName());
+	
+	//path to index files
+	private String indexDir;
+	
+	//path to files folder
+	private String dataDir;
+	
+	//IndexWriter to create and maintain index
+	private IndexWriter writer;
+	
+	public Indexer (String indexDir) throws IOException {
+		this.indexDir = indexDir;
+		
+		//create Directory to store indexes, use FSDirectory.open
+		//to automatically pick the most suitable directory implementation
+		Directory dir = FSDirectory.open(new File(indexDir));
+		
+		//create StandardAnalyzer to tokenize which uses default stop words 
+		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_46);
+		
+		//create configuration for new IndexWriter 
+		IndexWriterConfig conf = new IndexWriterConfig(Version.LUCENE_46, analyzer);
+		
+		//create IndexWriter, which can create new index, or
+		//adds, removes, or updates documents in the index
+		//however, it can not read or search
+		this.writer = new IndexWriter(dir, conf);
+	}
+	
+	//index files in directory 
+	//start with *.txt files, will change later after having crawler
+	public int index () throws Exception {
+		long start = System.currentTimeMillis();
+		Collection<File> files = FileUtils.listFiles(new File(this.dataDir), null, true);
+		
+		for (File f: files) {
+			indexFile(f);
+		}
+		long end = System.currentTimeMillis();
+		log.info("Indexing " + this.writer.numDocs() + " files took "
+			      + (end - start) + " milliseconds");
+		return this.writer.numDocs();
+	}
+	
+	//add Document to the index
+	public void indexFile(File f) throws Exception {
+		//log.info("Indexing " + f.getCanonicalPath());
+		Document doc = getDocument(f);
+		writer.addDocument(doc);
+	}
+	
+	//create Document and Fields
+	private Document getDocument(File f) throws Exception {
+		Document doc = new Document();
+		doc.add(new TextField("contents", new FileReader(f)));
+		doc.add(new StringField("filename", f.getName(), Field.Store.YES));
+		doc.add(new StringField("fullpath", f.getCanonicalPath(), Field.Store.YES));
+		
+		return doc;
+	}
+	
+	//close the IndexWriter, which means committing the indexes
+	public void close() throws IOException {
+		this.writer.close();
+	}
+	
+	public String getIndexDir() {
+		return indexDir;
+	}
+
+	public void setIndexDir(String indexDir) {
+		this.indexDir = indexDir;
+	}
+
+	public String getDataDir() {
+		return dataDir;
+	}
+
+	public void setDataDir(String dataDir) {
+		this.dataDir = dataDir;
+	}
+
+	public IndexWriter getWriter() throws IOException {
+		return new IndexWriter(FSDirectory.open(new File(indexDir)), 
+				new IndexWriterConfig(Version.LUCENE_46, new StandardAnalyzer(Version.LUCENE_46)));
+	}
+
+	public void setWriter(IndexWriter writer) {
+		this.writer = writer;
+	}
+}
---
+++ src/test/java/linkservice/index/IndexerTest.java
@@ -0,0 +1,71 @@
+package linkservice.index;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import linkservice.index.Indexer;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class IndexerTest {
+	static Logger log = Logger.getLogger(IndexerTest.class.getName());
+	
+	public static Indexer indexer;
+	public static String indexDir;
+	public static String dataDir;
+	
+	@BeforeClass
+	public static void setUp() throws Exception {
+		indexDir = "src/test/resources/index";
+		dataDir = "src/test/resources/samples/data/test";
+		
+		//empty directory
+		FileUtils.cleanDirectory(new File(indexDir));
+		
+		indexer = new Indexer(indexDir);
+		indexer.setDataDir(dataDir);
+		indexer.index();
+		indexer.close();
+	}
+	
+	//verify writer document count
+	@Test
+	public void test1IndexWriter() throws Exception {
+		log.info("Inside testIndexWriter()");
+		
+		Collection<File> files = FileUtils.listFiles(new File(dataDir), null, true);
+		IndexWriter writer = indexer.getWriter();
+		assertEquals("writer document count is not the same", files.size(), writer.numDocs());
+		writer.close();
+	}
+	
+	//verify reader document count
+	@Test
+	public void test2IndexReader() throws IOException {
+		log.info("Inside testIndexReader()");
+		
+		Collection<File> files = FileUtils.listFiles(new File(dataDir), null, true);
+		Directory directory = FSDirectory.open(new File(indexDir));
+		IndexReader reader = DirectoryReader.open(directory);
+		
+		assertEquals("reader document count is not the same", files.size(), reader.maxDoc());
+		assertEquals("reader document count is not the same", files.size(), reader.numDocs());
+		
+		reader.close();
+	}
+}
