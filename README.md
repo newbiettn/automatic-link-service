7959138a63bf20e148073f85b098f67d38054a93
newbiettn <newbiettn@gmail.com>
2015-02-02 20:36:17 +0100

    Initialize the project

---
+++ README.md
@@ -0,0 +1,1 @@
+The application is to enhance document searching and retrieving using text mining techniques as well as association trails inspired by Vannevar Bush's vision.
---
+++ src/main/java/linkservice/app/App.java
@@ -0,0 +1,13 @@
+package linkservice.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
